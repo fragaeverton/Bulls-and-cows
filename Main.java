d61c5f5a4d3bb5e4eaee60685862c5ece572b37f
fragaeverton <evertonfraga90@hotmail.com>
2022-01-11 13:06:41 +0000

    5 / 7

--- Main.java
+++ Main.java
@@ -1,32 +1,24 @@
 package bullscows;
 
 
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 import static bullscows.GameOutput.*;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
         chooseGameLevelMessage();
 
         int length = scanner.nextInt();
-        String secretCode = "";
         if (length > 10) {
             outOfLimitMessage(length);
         } else {
             startMessage();
-            while (secretCode.length() < length) {
-                secretCode = pseudoNumberGenerate(length);
-            }
-            startGame(secretCode);
+            startGame(randomGenerator(length));
         }
-
-
     }
 
     private static void startGame(String secretCode) {
@@ -38,19 +30,14 @@ public class Main {
         }
     }
 
-    private static String pseudoNumberGenerate(int number) {
-        String pseudoOne = String.valueOf(System.nanoTime());
-        char[] chars = pseudoOne.toCharArray();
-        Set<Character> charSet = new LinkedHashSet<>();
-        for (char c : chars) {
-            charSet.add(c);
-        }
+    private static String randomGenerator(int number) {
+        List<Character> numbers = Arrays.asList('0','1','2','3','4','5','6','7','8','9');
         StringBuilder sb = new StringBuilder();
-        for (Character character : charSet) {
-            sb.append(character);
+        while (numbers.get(0) == '0') {
+            Collections.shuffle(numbers);
         }
-        while (sb.charAt(0) == 0) {
-            sb.deleteCharAt(0);
+        for (Character character : numbers) {
+            sb.append(character);
         }
         return sb.substring(0, number);
     }
